a54331d72a9fa30a6317b5163878476b0dabea73
Liam Makena Monninger <liam@ramate.io>
2025-06-11 17:27:45 +0800

    fix: after help bug.

--- src/lib.rs
+++ src/lib.rs
@@ -363,8 +363,13 @@ fn build_command_markdown(
         }
     }
 
+    // There's a bug in the clap upstream that will cause after_long_help to be empty instead of None. This to checks if empty so that we don't short circuit if get_after_help is available. 
     if let Some(help) = command.get_after_long_help() {
-        writeln!(buffer, "{}\n", help)?;
+        if !help.is_empty() {
+            writeln!(buffer, "{}\n", help)?;
+        } else if let Some(help) = command.get_after_help() {
+            writeln!(buffer, "{}\n", help)?;
+        }
     } else if let Some(help) = command.get_after_help() {
         writeln!(buffer, "{}\n", help)?;
     }
